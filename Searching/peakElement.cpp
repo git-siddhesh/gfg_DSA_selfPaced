a7e10b1b31528239b6cecc2fe26e793c6b32a577
git-siddhesh <51389823+git-siddhesh@users.noreply.github.com>
2020-05-07 05:04:30 +0530

    Create peakElement.cpp

---
+++ Searching/peakElement.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+/* The function should return the index of any
+   peak element present in the array */
+
+int peakElement(int arr[], int l, int r, int n)
+{
+    if(l<=r)
+    {
+        int mid = l+(r-l)/2;
+        if ((mid == 0 || arr[mid] >= arr[mid-1]) && 
+            (mid ==n-1 || arr[mid]>= arr[mid+1]))
+             return mid;
+        if (arr[mid] < arr[mid-1])
+            return peakElement(arr,l,mid-1,n);
+        if (arr[mid] < arr[mid+1])
+            return peakElement(arr,mid+1,r,n);
+    }
+    return -1;
+    
+}
+// arr: input array
+// n: size of array
+int peakElement(int arr[], int n)
+{
+   // Your code here
+   int p = peakElement(arr,0,n-1,n);
+   return p;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		bool f=0;
+		int A = peakElement(a,n);
+		
+		if(n==1)
+		f=1;
+		else
+		if(A==0 and a[0]>=a[1])
+		f=1;
+		else if(A==n-1 and a[n-1]>=a[n-2])
+		f=1;
+		else if(a[A] >=a[A+1] and a[A]>= a[A-1])
+		f=1;
+		else
+		f=0;
+		
+		cout<<f<<endl;
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
